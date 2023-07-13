2537f50001652cfbde1f6c49414951e6f63b12a1
Mwadali <wellingtonmwadali@gmail.com>
2023-07-13 14:39:43 -0700

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""model for the Amenity """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Implementation for the Amenity model"""
+    name = ""
