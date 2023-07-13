ab3390c8886db5686130d671529083568285687a
Mwadali <wellingtonmwadali@gmail.com>
2023-07-13 14:35:46 -0700

    city.py

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for the City model"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Implementation of the City class"""
+    state_id = ""
+    name = ""
