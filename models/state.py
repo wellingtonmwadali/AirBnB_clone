2997c7c6d619869f517f277320ed384d7f92c273
Mwadali <wellingtonmwadali@gmail.com>
2023-07-13 14:32:02 -0700

    state.py

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module for the State model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ State model for any state object"""
+    name = ""
