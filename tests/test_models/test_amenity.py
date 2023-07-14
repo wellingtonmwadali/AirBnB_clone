93e4902da546553c9ea7b57c5fe5af1da2a74314
Alex Maina <lexizgichbuoy@gmail.com>
2023-07-14 06:23:06 -0700

    tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""unittest for Amenity calss"""
+import unittest
+
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """testcase for Amenity class"""
+
+    def setUp(self):
+        self.amenity = Amenity()
+
+    def test_amenity(self):
+        self.assertTrue(issubclass(type(self.amenity), BaseModel))
+
+    def test_attr(self):
+        self.assertTrue(hasattr(self.amenity, "name"))
+
+    def test_class_attr(self):
+        self.assertIs(type(self.amenity.name), str)
+        self.assertFalse(bool(getattr(self.amenity, "name")))
+
+if __name__ == "__main__":
+    unittest.main()
